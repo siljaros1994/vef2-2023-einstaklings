d3149d9653b556e980cb431ffff69c0929ed9157
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-04 14:11:26 +0000

    Create SearchBar.js

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onWeatherData }) => {
+  const [location, setLocation] = useState('');
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    const API_KEY = "f5fec0946bc550184dec442698c35d67";
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric&lang=is`);
+    const data = await response.json();
+
+    onWeatherData(data);
+  };
+
+  return (
+    <form className="form-inline justify-content-center" onSubmit={handleSearch}>
+      <input
+        type="text"
+        className="form-control mr-2"
+        placeholder="Sláðu inn staðsetningu"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button type="submit" className="btn btn-primary">
+        Leita
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
