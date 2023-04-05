a207f9ac586a7f418937027486209e3eb9918d5c
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-05 02:48:15 +0000

    Update SearchBar.js

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -3,30 +3,44 @@ import '../css/styles.css';
 
 const SearchBar = ({ onWeatherData }) => {
   const [location, setLocation] = useState('');
+  const [error, setError] = useState(null);
 
   const handleSearch = async (e) => {
-    e.preventDefault();
+    if (e.key === 'Enter' || e.type === 'submit') {
+      e.preventDefault();
+      setError(null);
 
-    const API_KEY = "f5fec0946bc550184dec442698c35d67";
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric&lang=is`);
-    const data = await response.json();
-
-    onWeatherData(data);
+      const API_KEY = "f5fec0946bc550184dec442698c35d67";
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric&lang=is`);
+      if (!response.ok) {
+        setError("Staðsetningin finnst ekki");
+        return;
+      }
+      const data = await response.json();
+  
+      onWeatherData(data);
+    }
   };
 
   return (
-    <form className="form-inline justify-content-center" onSubmit={handleSearch}>
-      <input
-        type="text"
-        className="form-control mr-2"
-        placeholder="Sláðu inn staðsetningu"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-      />
-      <button type="submit" className="btn btn-primary">
-        Leita
-      </button>
-    </form>
+    <div className="card">
+      <div className="card-header">
+        <h5 className="card-title">Veður- og hitastigsstöð</h5>
+      </div>
+      <div className="card-body">
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Sláðu inn staðsetningu"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            onKeyPress={handleSearch}
+          />
+        </div>
+        {error && <div className="error-message">{error}</div>}
+      </div>
+    </div>
   );
 };
 
