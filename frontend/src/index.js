4df4b008e11c8392eb3c13f6648999932f55b3fc
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-05 02:48:25 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; 
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
