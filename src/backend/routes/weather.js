682b6222e3750590ac99c5f905de57980781e34e
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-08 17:49:57 +0000

    Create weather.js

---
+++ src/backend/routes/weather.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+const API_KEY = "f5fec0946bc550184dec442698c35d67";
+
+router.get('/:location', async (req, res) => {
+  const { location } = req.params;
+  try {
+      const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`);
+      res.json(weatherResponse.data);
+  } catch (error) {
+      res.status(500).json({ error: 'Villa við að sækja veðurupplýsingar' });
+  }
+});
+
+router.get('/lat/:lat/lon/:lon', async (req, res) => {
+  const { lat, lon } = req.params;
+  try {
+      const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+      res.json(weatherResponse.data);
+  } catch (error) {
+      res.status(500).json({ error: 'Villa við að sækja veðurupplýsingar' });
+  }
+});
+
+module.exports = router;
