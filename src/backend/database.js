2064eb105fefe3a0724ab1b8906cea140a7e0192
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-08 19:38:26 +0000

    Create database.js

---
+++ src/backend/database.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
