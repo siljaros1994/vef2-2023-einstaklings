8426e2f001bfd9a26d326f7657383b4ee3c0258c
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-08 17:49:37 +0000

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
-const API_KEY = 'f5fec0946bc550184dec442698c35d67';
+//const API_KEY = 'f5fec0946bc550184dec442698c35d67';
+
+const API_URL = "http://localhost:3001/api/weather";
 
 export const fetchWeatherDataByLocation = async (location) => {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`
-  );
+  const response = await fetch(`${API_URL}/${location}`);
 
   if (!response.ok) {
     throw new Error('Failed to fetch weather data');
@@ -13,9 +13,7 @@ export const fetchWeatherDataByLocation = async (location) => {
 };
 
 export const fetchWeatherDataByCoordinates = async (lat, lon) => {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
-  );
+  const response = await fetch(`${API_URL}/lat/${lat}/lon/${lon}`);
 
   if (!response.ok) {
     throw new Error('Failed to fetch weather data');
