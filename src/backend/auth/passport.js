5c389f5326cf8ca746892c7e959f8a931c9802d3
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-09 18:15:10 +0000

    Create passport.js

---
+++ src/backend/auth/passport.js
@@ -0,0 +1,91 @@
+import passport from 'passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { findById } from './users.js';
+
+const { JWT_SECRET: jwtSecret, TOKEN_LIFETIME: tokenLifetime = 3600 } = process.env;
+
+if (!jwtSecret) {
+  console.error('Vantar .env gildi');
+  process.exit(1);
+}
+
+async function strat(data, next) {
+  const user = await findById(data.id);
+
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+}
+
+function requireAuthentication(req, res, next) {
+  return passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      const error = info.name === 'TokenExpiredError' ? 'expired token' : 'invalid token';
+
+      return res.status(401).json({ error });
+    }
+
+    req.user = user;
+    return next();
+  })(req, res, next);
+}
+
+function addUserIfAuthenticated(req, res, next) {
+  return passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (user) {
+      req.user = user;
+    }
+
+    return next();
+  })(req, res, next);
+}
+
+function requireAdmin(req, res, next) {
+  return passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      const error = info.name === 'TokenExpiredError' ? 'expired token' : 'invalid token';
+
+      return res.status(401).json({ error });
+    }
+
+    if (!user.admin) {
+      const error = 'insufficient authorization';
+      return res.status(401).json({ error });
+    }
+
+    req.user = user;
+    return next();
+  })(req, res, next);
+}
+
+const tokenOptions = { expiresIn: parseInt(tokenLifetime, 10) };
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: jwtSecret
+};
+
+passport.use(new Strategy(jwtOptions, strat));
+
+export {
+  requireAuthentication,
+  addUserIfAuthenticated,
+  requireAdmin,
+  tokenOptions,
+  jwtOptions,
+  passport
+};
