9e54fe0fbef565de2f9bd87b143da30716a2e50c
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-09 18:14:56 +0000

    Create insert.sql

---
+++ sql/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email, password) VALUES ('Anna', 'anna@example.com', 'password123');
+INSERT INTO users (name, email, password) VALUES ('Jane', 'jane@example.com', 'password456');
