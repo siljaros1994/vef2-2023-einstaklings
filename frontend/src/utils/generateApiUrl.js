586774089e12c0087e83da3b4600bbc3ed2cc89a
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-13 19:22:51 +0000

    Update generateApiUrl.js

--- frontend/src/utils/generateApiUrl.js
+++ frontend/src/utils/generateApiUrl.js
@@ -1,9 +1,20 @@
 export function generateApiUrl(path) {
-    const baseUrl = process.env.REACT_APP_API_URL;
-    console.log(baseUrl)
-    const url = new URL(path, baseUrl);
-  
-    console.log('url', url);
-  
-    return url;
+  const baseUrl = process.env.REACT_APP_API_URL;
+  console.log(baseUrl)
+
+  if (!baseUrl) {
+      console.error('Error: REACT_APP_API_URL environment variable is not defined.');
+      return;
   }
+
+  try {
+      const url = new URL(path, baseUrl);
+      console.log('url', url);
+      return url;
+  } catch (error) {
+      console.error(`Error generating API URL: ${error.message}`);
+      console.error(`Base URL: ${baseUrl}`);
+      console.error(`Path: ${path}`);
+      throw error;
+  }
+}
