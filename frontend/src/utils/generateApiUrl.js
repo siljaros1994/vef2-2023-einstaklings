6f41acdb83f21f24738c6c8ec572cb585ab5347f
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-13 16:34:02 +0000

    Create generateApiUrl.js

---
+++ frontend/src/utils/generateApiUrl.js
@@ -0,0 +1,9 @@
+export function generateApiUrl(path) {
+    const baseUrl = process.env.REACT_APP_API_URL;
+    console.log(baseUrl)
+    const url = new URL(path, baseUrl);
+  
+    console.log('url', url);
+  
+    return url;
+  }
