50d335625687475041759f4c3ae4c61358ba63f0
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-19 19:51:48 +0000

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (name, email, password) VALUES ('Anna', 'anna@example.com', 'password123');
-INSERT INTO users (name, email, password) VALUES ('Jane', 'jane@example.com', 'password456');
+INSERT INTO users (id, username, email, password, admin) VALUES (1, 'admin', 'admin@example.com', '$2b$10$tMh16IVifa87x08tpgYCmuoM/fOHLPRu02xX.Jy7luixIFYqsOlfC', true);
+INSERT INTO users (id, username, email, password, admin) VALUES (2, 'Anna', 'anna@example.com', '$2b$10$4k2iW7JwYA49D.jJ4RczauleLKa7Ea09BRpBHZE4M0uS4ytVe7xHO', false);
