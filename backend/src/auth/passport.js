e16924240996c97261c8b5cc6eb474a4aac30b6b
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-19 19:50:21 +0000

    Update passport.js

--- backend/src/auth/passport.js
+++ backend/src/auth/passport.js
@@ -1,84 +1,35 @@
 import passport from 'passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { findById } from './users.js';
+import { Strategy as LocalStrategy } from 'passport-local';
+import bcrypt from 'bcrypt';
+import { findByEmail } from '../middlewares/users.js';
 
-const { JWT_SECRET: jwtSecret, TOKEN_LIFETIME: tokenLifetime = 3600 } = process.env;
-
-if (!jwtSecret) {
-  console.error('Vantar .env gildi');
-  process.exit(1);
-}
-
-async function strat(data, next) {
-  const user = await findById(data.id);
-
-  if (user) {
-    next(null, user);
-  } else {
-    next(null, false);
-  }
-}
-
-export function requireAuthentication(req, res, next) {
-  return passport.authenticate('jwt', { session: false }, (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    const user = await findByEmail(email);
 
     if (!user) {
-      const error = info.name === 'TokenExpiredError' ? 'expired token' : 'invalid token';
-
-      return res.status(401).json({ error });
+      return done(null, false, { message: 'Incorrect email or password.' });
     }
 
-    req.user = user;
-    return next();
-  })(req, res, next);
-}
-
-export function addUserIfAuthenticated(req, res, next) {
-  return passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err) {
-      return next(err);
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Incorrect email or password.' });
+      }
+    } catch (err) {
+      return done(err);
     }
+  })
+);
 
-    if (user) {
-      req.user = user;
-    }
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
-    return next();
-  })(req, res, next);
-}
-
-export function requireAdmin(req, res, next) {
-  return passport.authenticate('jwt', { session: false }, (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-
-    if (!user) {
-      const error = info.name === 'TokenExpiredError' ? 'expired token' : 'invalid token';
-
-      return res.status(401).json({ error });
-    }
-
-    if (!user.admin) {
-      const error = 'insufficient authorization';
-      return res.status(401).json({ error });
-    }
-
-    req.user = user;
-    return next();
-  })(req, res, next);
-}
-
-export const tokenOptions = { expiresIn: parseInt(tokenLifetime, 10) };
-
-export const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: jwtSecret
-};
-
-passport.use(new Strategy(jwtOptions, strat));
+passport.deserializeUser(async (id, done) => {
+  const user = await findById(id);
+  done(null, user);
+});
 
 export default passport;
