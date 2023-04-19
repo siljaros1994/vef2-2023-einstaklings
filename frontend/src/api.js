165920cc9d9cf2fdaca6fcb263fd4b1c970842db
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-19 19:51:12 +0000

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import { generateApiUrl } from './utils/generateApiUrl';
 
 const API_URL = generateApiUrl("/api/weather");
+const AUTH_API_URL = generateApiUrl("/auth");
 
 export const fetchWeatherDataByLocation = async (location) => {
   const response = await fetch(`${API_URL}/${location}`, { credentials: 'include' });
@@ -17,6 +18,40 @@ export const fetchWeatherDataByCoordinates = async (lat, lon) => {
 
   if (!response.ok) {
     throw new Error('Failed to fetch weather data');
+  }
+
+  return await response.json();
+};
+
+export const loginUser = async (email, password) => {
+  const response = await fetch(`${AUTH_API_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to log in');
+  }
+
+  return await response.json();
+};
+
+export const registerUser = async (email, password) => {
+  const response = await fetch(`${AUTH_API_URL}/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to register');
   }
 
   return await response.json();
