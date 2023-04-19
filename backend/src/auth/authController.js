47f7c78588c6db185107c3ced967fca3348fbed7
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-19 19:50:08 +0000

    Update authController.js

--- backend/src/auth/authController.js
+++ backend/src/auth/authController.js
@@ -1,6 +1,5 @@
-import jwt from 'jsonwebtoken';
-import { createUser, comparePasswords, findByUsername } from './users.js';
-import { jwtOptions, tokenOptions } from './passport.js';
+import bcrypt from 'bcrypt';
+import { createUser, findByEmail } from '../middlewares/users.js';
 
 async function register(req, res) {
   const { username, email, password } = req.body;
@@ -9,14 +8,15 @@ async function register(req, res) {
     return res.status(400).json({ error: 'Missing username, email, or password' });
   }
 
-  const existingUser = await findByUsername(username);
+  const existingUser = await findByEmail(email);
 
   if (existingUser) {
-    return res.status(400).json({ error: 'Username is already taken' });
+    return res.status(400).json({ error: 'Email is already taken' });
   }
 
   try {
-    const user = await createUser(username, email, password);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await createUser(username, email, hashedPassword);
 
     return res.status(201).json({ user });
   } catch (error) {
@@ -24,29 +24,32 @@ async function register(req, res) {
   }
 }
 
-async function login(req, res) {
-  const { username, password } = req.body;
+async function login(req, res, next) {
+  const { email, password } = req.body;
 
-  if (!username || !password) {
-    return res.status(400).json({ error: 'Missing username or password' });
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Missing email or password' });
   }
 
-  const user = await findByUsername(username);
+  const user = await findByEmail(email);
 
   if (!user) {
-    return res.status(401).json({ error: 'Invalid username or password' });
+    return res.status(401).json({ error: 'Invalid email or password' });
   }
+  console.log(user.password)
 
-  const isCorrectPassword = await comparePasswords(password, user.password);
+  const isCorrectPassword = await bcrypt.compare(password, user.password);
 
   if (!isCorrectPassword) {
-    return res.status(401).json({ error: 'Invalid username or password' });
+    return res.status(401).json({ error: 'Invalid email or password' });
   }
 
-  const payload = { id: user.id, username: user.username };
-  const token = jwt.sign(payload, jwtOptions.secretOrKey, tokenOptions);
-
-  return res.status(200).json({ token, user });
+  req.login(user, (err) => {
+    if (err) {
+      return next(err);
+    }
+    return res.status(200).json({ message: 'Logged in successfully', user });
+  });
 }
 
 export { register, login };
