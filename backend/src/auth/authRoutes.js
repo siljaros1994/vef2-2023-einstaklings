4e85d6554a9f0078f311bfcc6688702300b0ff73
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-04-19 19:50:12 +0000

    Update authRoutes.js

--- backend/src/auth/authRoutes.js
+++ backend/src/auth/authRoutes.js
@@ -1,10 +1,14 @@
 //import { createUser } from './users.js';
 import express from 'express';
-import { login, register } from './authController.js';
+import { requireAuth, checkUserIsAdmin } from '../middlewares/auth.js';
+import { register, login } from '../auth/authController.js';
 
 const router = express.Router();
 
-router.post('/login', login);
 router.post('/register', register);
+router.post('/login', login);
+router.get('/admin', requireAuth, checkUserIsAdmin, (req, res) => {
+  res.status(200).json({ message: 'Admin route' });
+});
 
 export default router;
